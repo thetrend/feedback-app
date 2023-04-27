e1e3d216656572c3b86db69173298a06cf43cc0d
Grace de la Mora <thetrend@users.noreply.github.com>
2023-04-27 10:13:32 -0400

    initial cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
+import Header from "./components/Header";
+
+const App = () => {
   return (
     <>
+      <Header />
       <h1>Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
